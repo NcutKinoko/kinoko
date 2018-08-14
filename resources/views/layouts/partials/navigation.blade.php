f9ce3e851dcdad9cf693ef8cf8d24eb005a42a7d
a0952288480 <a0952288480@gmail.com>
2018-08-14 17:46:01 +0800

    加上登入跳轉以及登出

--- resources/views/layouts/partials/navigation.blade.php
+++ resources/views/layouts/partials/navigation.blade.php
@@ -27,6 +27,7 @@
             text-decoration: none;
             border-radius: 10px;
             padding: 14px 50px;
+            display: inline-block;
         }
 
         .number > li > a:hover {
@@ -35,11 +36,11 @@
             text-decoration: none;
         }
 
-        .dropdown-fuck {
+        .dropdown-right-top {
             cursor: pointer;
         }
 
-        .dropdown-fuck > ul {
+        .dropdown-right-top > ul {
             list-style-type: none;
             margin: 0;
             padding: 0;
@@ -50,17 +51,23 @@
             box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
         }
 
-        .dropdown-fuck > ul > li {
+        .dropdown-right-top > ul > li {
             padding: 12px 16px;
-            right: 0;
+            position: relative;
+            left: 0;
         }
 
-        .dropdown-fuck:hover > ul {
+        .dropdown-right-top > ul > li>a {
+            text-decoration: none;
+            color: black;
+        }
+
+        .dropdown-right-top:hover > ul {
             display: block;
         }
 
-        .dropdown-fuck>ul>li:hover {
-            background-color: gray;
+        .dropdown-right-top>ul>li:hover {
+            background-color: #f9f9f9;
         }
 
         .list {
@@ -95,22 +102,28 @@
 
     <!--navigation-->
     <div>
+        <ul class="number">
+            <li style="float: left;padding: 14px 50px;font-size: large;">新社香菇</li>
         @if(Auth::check())
-        @else
-            <ul class="number">
-                <li><a href="#register" class="number-li-a">註冊</a></li>
-                <li><a href="#login" class="number-li-a">登入</a></li>
-                <li class="dropdown-fuck"
+                <li class="dropdown-right-top"
                     style="color: black;text-decoration: none;padding: 14px 50px;border-radius: 10px;">
-                    登出<i class="fa fa-caret-down"></i>
+                    登出 <i class="fa fa-caret-down"></i>
                     <ul>
-                        <li>登出</li>
+                        <li><a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                登出
+                            </a>
+                        </li>
                     </ul>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
                 </li>
-                <li style="float: left;padding: 14px 50px;font-size: large;">新社香菇</li>
-            </ul>
+        @else
+                <li><a href="{{ route('register') }}" >註冊</a></li>
+                <li><a href="{{ route('login') }}" >登入</a></li>
         @endif
-
+        </ul>
         <ul class="list">
             <li class="list-li"><a href="#index" class="list-li-a">首頁</a></li>
             <li class="list-li"><a href="#history" class="list-li-a">歷史</a></li>
