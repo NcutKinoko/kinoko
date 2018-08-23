8937f4ca0eb6373d677faa8503a2769902b838ff
a0952288480 <a0952288480@gmail.com>
2018-08-23 17:22:00 +0800

    為了修改修改密碼email的view而新增的檔案

--- app/Notifications/BackstageResetPasswordNotification.php
+++ app/Notifications/BackstageResetPasswordNotification.php
@@ -42,6 +42,8 @@ class BackstageResetPasswordNotification extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
+            ->subject('新社香菇網站會員密碼修改')
+            ->greeting('您好!')
             ->line('這是一封密碼重置郵件，如果是您本人操作，請點擊以下按鈕繼續：')
             ->action('重置密碼', url(config('app.url').route('Backstage.show.reset', $this->token, false)))
             ->line('如果您並沒有執行此操作，您可以選擇忽略此郵件。');
