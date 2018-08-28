5e7e741d825c462f1ce3350879f0cc4853b962d7
a0952288480 <a0952288480@gmail.com>
2018-08-29 00:01:18 +0800

    還是沒用好

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -9,18 +9,27 @@
             width: 100%;
             height: 110px;
         }
-        .product-list {
-            width: 80%;
-            background-color: black;
+        .product
+        {
+            width: 100%;
+            height: 350px;
             text-align: center;
         }
+        .product-list {
+            width: 80%;
+            height: 80%;
+            padding: 0;
+            background-color: black;
+            margin:0 auto;
+        }
         .product-list > ul{
+            text-align: center;
             list-style: none;
         }
         .product-list > ul > li{
             width: 400px;
             height: 400px;
-            float: left;
+            display: inline-block;
         }
         .img-size{
             width:300px;
@@ -30,7 +39,7 @@
     <div class="padding">
 
     </div>
-    <div class="wrapper">
+    <div class="product">
             <div class="product-list">
                 <ul>
                     <li><img class="img-size" src="https://i.ytimg.com/vi/bLltMkKxmYA/maxresdefault.jpg"></li>
