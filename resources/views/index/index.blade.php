86115ff2c511503a709cdff089cd165c5adad7b6
a0952288480 <a0952288480@gmail.com>
2018-08-29 11:54:01 +0800

    首頁修改

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -12,23 +12,29 @@
         .product
         {
             width: 100%;
-            height: 350px;
+            height: 450px;
             text-align: center;
         }
         .product-list {
-            width: 80%;
-            height: 80%;
-            padding: 0;
+            width: 90%;
+            height: 100%;
+            margin: 0 auto;
+            padding: 0 70px;
             background-color: black;
-            margin:0 auto;
+            display:inline-block;
+        }
+        .product-list > h1
+        {
+            color: #FFFFFF;
         }
         .product-list > ul{
             text-align: center;
             list-style: none;
+            padding: 15px;
         }
         .product-list > ul > li{
-            width: 400px;
-            height: 400px;
+            width:350px;
+            height: 350px;
             display: inline-block;
         }
         .img-size{
@@ -41,6 +47,8 @@
     </div>
     <div class="product">
             <div class="product-list">
+                <h1>產品</h1>
+                <h1>PRODUCT</h1>
                 <ul>
                     <li><img class="img-size" src="https://i.ytimg.com/vi/bLltMkKxmYA/maxresdefault.jpg"></li>
                     <li><img class="img-size" src="https://i.ytimg.com/vi/bLltMkKxmYA/maxresdefault.jpg"></li>
