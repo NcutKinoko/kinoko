b3e49ef8221c257ba6a827051b1010c0c58857da
a0952288480 <a0952288480@gmail.com>
2018-09-30 23:13:56 +0800

    嘗試新社頁面前端

---
+++ resources/views/xinshe/list.blade.php
@@ -0,0 +1,9 @@
+<head>
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+</head>
+
+<body>
+<div class="container">
+
+</div>
+</body>
