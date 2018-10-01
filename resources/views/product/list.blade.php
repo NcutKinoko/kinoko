738e5454d4704f2a0cd57bd0399f81322bed3de6
a0952288480 <a0952288480@gmail.com>
2018-10-01 18:03:07 +0800

    加回被砍掉的js資源引用

--- resources/views/product/list.blade.php
+++ resources/views/product/list.blade.php
@@ -8,6 +8,8 @@
         <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <link href="{{asset('css/Product-List.css')}}" rel="stylesheet">
         <script src="{{asset('js/Product-List.js')}}"></script>
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
     </head>
     <!------ Include the above in your HEAD tag ---------->
     <div class="container">
