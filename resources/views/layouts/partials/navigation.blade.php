4492209c4eb5d564fff90fa23834aaa6438a4a4b
a0952288480 <a0952288480@gmail.com>
2018-10-29 10:10:52 +0800

    解決換頁時navigation圖片跑掉問題

--- resources/views/layouts/partials/navigation.blade.php
+++ resources/views/layouts/partials/navigation.blade.php
@@ -8,15 +8,15 @@
         <li><a href="">新社香菇</a></li>
     </ul>
     <ul class="headinfo">
-        <li style="border-right: none"><a href="" title="YotTube"><img alt="YotTube" src="../img/icon/youtube.png"
+        <li style="border-right: none"><a href="" title="YotTube"><img alt="YotTube" src="{{url('../img/icon/youtube.png')}}"
                                                                        height="25px" width="25px"></a></li>
-        <li><a href="" title="粉絲專頁連結"><img alt="粉絲專頁連結" src="../img/icon/FB.png" height="25px" width="25px"></a></li>
-        <li><a href="" title="購物車"><img alt="購物車" src="../img/icon/shoppingcart.png" height="25px" width="25px"></a>
+        <li><a href="" title="粉絲專頁連結"><img alt="粉絲專頁連結" src="{{url('../img/icon/FB.png')}}" height="25px" width="25px"></a></li>
+        <li><a href="" title="購物車"><img alt="購物車" src="{{url('../img/icon/shoppingcart.png')}}" height="25px" width="25px"></a>
         </li>
-        <li><a href="" title="會員專區"><img alt="會員專區" src="../img/icon/man.png" height="25px" width="25px"></a></li>
+        <li><a href="" title="會員專區"><img alt="會員專區" src="{{url('../img/icon/man.png')}}" height="25px" width="25px"></a></li>
     </ul>
     <ul class="headstyle">
-        <li><a href="">首頁</a></li>
+        <li><a href="{{route('show.index')}}">首頁</a></li>
         <li><a href="">最新消息</a></li>
         <li><a href="{{route('product.list')}}">產品介紹</a></li>
         <li><a href="">歷史簡介</a></li>
